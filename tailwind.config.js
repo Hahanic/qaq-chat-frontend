feb50748813fad3b44c81cdfa9ccf8d7fed991ee
Hahanic <nickjinxing@gmail.com>
2025-05-29 16:27:56 +0800

    保存升级前的本地修改

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,3 @@
-import daisyui from 'daisyui'
-
  /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -8,9 +6,5 @@ export default {
   ],
    theme: {
      extend: {},
-   },
-   plugins: [daisyui],
-   daisyui: {
-     themes: ['light', 'dark', 'cupcake', 'forest', 'retro'],
    },
  }
